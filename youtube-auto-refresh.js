d84db24f108a4744dc6e4196d38ff315a03c2697
E-GRAU <60931374+E-GRAU@users.noreply.github.com>
2023-05-06 18:01:11 -0500

    Create youtube-auto-refresh.js

---
+++ youtube-auto-refresh.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         Youtube Auto-Refresh Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com*
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    // Your code here...
+
+    let counter = 0;
+    let href = window.location.href;
+
+
+    function isVideo() {
+        const params = new URLSearchParams(window.location.search);
+        return params.get("v");
+    }
+
+    function refreshPage() {
+        const params = new URLSearchParams(window.location.search);
+
+        params.delete("t");
+        params.delete("list");
+        params.delete("start_radio");
+        window.location.search = params.toString()
+    }
+
+    function redirectToMain() {
+        window.location.href = "https://www.youtube.com/";
+    }
+
+    window.addEventListener('popstate', function(event) {
+        // The URL changed...
+        console.log("The URL changed...", window.location.href);
+    });
+
+    window.onload = function() {
+
+
+
+
+        setInterval(() => {
+
+            counter++;
+            //console.log("counter: " + counter)
+            console.log(`time: ${Math.round(counter/60)}m ${counter%60}s counter: ${counter}`)
+            //4 14
+
+            if (href !== window.location.href) {
+                href = window.location.href;
+                counter = 0;
+            }
+
+            if (counter < 40 * 60) return;
+
+            if (!isVideo()) {
+                console.log("not video")
+                return;
+            }
+            let title = document.title.toLowerCase();
+            if (title.includes("cumpleaños") || title.includes("birthday")) {
+
+                redirectToMain();
+                return;
+            }
+
+            refreshPage();
+
+
+        }, 1 * 1000)
+    }
+})();
