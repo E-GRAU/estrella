11eea99391caf772e542ed37aa8005ac2dd7f3c1
E-GRAU <60931374+E-GRAU@users.noreply.github.com>
2023-05-06 18:31:53 -0500

    Update youtube-auto-refresh.js

--- youtube-auto-refresh.js
+++ youtube-auto-refresh.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Auto-Refresh Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.youtube.com*
