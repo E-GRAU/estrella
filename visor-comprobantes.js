933c477bb63bbd352778ec338646c8173c2b9ed2
E-GRAU <60931374+E-GRAU@users.noreply.github.com>
2023-05-20 15:07:21 -0500

    Create visor-comprobantes.js

---
+++ visor-comprobantes.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         Visor Factura/Boleta Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://comprobantes.facturactiva.com/Losayyick/visorComprobantes?tipo=factura
+// @match        https://comprobantes.facturactiva.com/Losayyick/visorComprobantes?tipo=boleta
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=facturactiva.com
+// @require      https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.11.7/dayjs.min.js
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    // Your code here...
+
+    const params = new URLSearchParams(window.location.search)
+    const tipo = params.get("tipo")
+    console.log("tipo", tipo)
+
+    $("#select-document-type").val("03")
+    $("#document-serial").val("B001")
+
+    if (tipo === "factura") {
+        $("#select-document-type").val("01")
+        $("#document-serial").val("F001")
+    }
+
+
+
+    $("#emission-date").val(dayjs().format("DD/MM/YYYY"))
+
+
+
+    $("#document-total").attr("type", "number").on("keydown", function(e) {
+        const key = e.which;
+
+        if (key == 38 || key == 40) {
+            $("#document-correlative").trigger("focus")
+        }
+
+        if (key == 13) {
+            $("#search-document")[0].click()
+
+        }
+
+    })
+
+
+    $("#document-correlative").attr("type", "number").on("keydown", function(e) {
+        const key = e.which;
+
+        if (key == 38 || key == 40 || key == 13) {
+            $("#document-total").trigger("focus")
+        }
+
+    })
+
+
+    $("#document-correlative").trigger("focus")
+})();
