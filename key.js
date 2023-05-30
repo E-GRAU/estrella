792d083d5f6289f11aa0767440984e4457b8f075
E-GRAU <60931374+E-GRAU@users.noreply.github.com>
2023-05-30 12:22:04 -0500

    Update key.js

--- key.js
+++ key.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Key Userscript
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.2.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://*/*
