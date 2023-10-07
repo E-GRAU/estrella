ce8e21c5b892eb015026dafb3b3ea5a490cd195b
E-GRAU <60931374+E-GRAU@users.noreply.github.com>
2023-10-07 17:08:17 -0500

    Update whakoom-list.js

--- whakoom-list.js
+++ whakoom-list.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.whakoom.com/jonathan17/lists/superman_infinity_frontier_57069
+// @match        https://www.whakoom.com/*/lists/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=whakoom.com
 // @grant        none
